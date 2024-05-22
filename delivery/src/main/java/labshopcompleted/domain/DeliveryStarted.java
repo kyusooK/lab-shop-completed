c69938dbc4ef4cde8842246205b42e624cbcc2ac
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2024-05-22 13:14:05 +0900

    commit

---
+++ delivery/src/main/java/labshopcompleted/domain/DeliveryStarted.java
@@ -0,0 +1,27 @@
+package labshopcompleted.domain;
+
+import labshopcompleted.domain.*;
+import labshopcompleted.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+import java.time.LocalDate;
+
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private String address;
+    private String customerId;
+    private Integer quantity;
+
+    public DeliveryStarted( aggregate){
+        super(aggregate);
+    }
+    public DeliveryStarted(){
+        super();
+    }
+}
+//>>> DDD / Domain Event
